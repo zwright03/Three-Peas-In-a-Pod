edcf35caeeb8f306e8efc3cfe270df109995a790
Zach Wright <zachwright03@gmail.com>
2024-12-02 14:34:51 -0500

    Changed window management, added a welcome window.

--- DSA-Project-3/src/main.cpp
+++ DSA-Project-3/src/main.cpp
@@ -119,151 +119,202 @@ int main() {
         return -1;
     }
 
-    sf::Text testText;
-    testText.setFont(font);
-    testText.setString("Test");
-    testText.setCharacterSize(50);
-    testText.setFillColor(sf::Color::Black);
-    testText.setPosition(500, 350);
+    { // WELCOME WINDOW
+        std::string welcomeStr = R"(Welcome to the sorting visualizer.
+This will be a comparison between the performance of
+the Merge sort and Quick sort algrotihms.
 
-    auto vis1 = std::make_shared<QuickVisualizer>(&window, &font, true); // right side
-    auto vis2 = std::make_shared<MergeVisualizer>(&window, &font, false); // left side
+Make this say something a little more meaningful that shows what
+we are trying to accomplish in this project
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
+Click anywhere to continue.)";
+        sf::Text welcomeText;
+        welcomeText.setFont(font);
+        welcomeText.setString(welcomeStr);
+        welcomeText.setCharacterSize(25);
+        welcomeText.setFillColor(sf::Color::White);
+        welcomeText.setOrigin(welcomeText.getLocalBounds().width / 2, welcomeText.getLocalBounds().height / 2);
+        welcomeText.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+
+        bool move_to_visualization = false;
+        while (window.isOpen() && !move_to_visualization) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+                if (event.type == sf::Event::MouseButtonPressed)
+                    move_to_visualization = true;
+            }
+
+            window.clear(sf::Color::Black);
+
+            window.draw(welcomeText);
+
+            window.display();
+        }
+    }
+
+    { // SORT VISUALIZATION WINDOW
+        sf::Text titleText;
+        titleText.setFont(font);
+        titleText.setString("Merge Sort vs. Quick Sort, Visualization:");
+        titleText.setCharacterSize(35);
+        titleText.setFillColor(sf::Color::White);
+        titleText.setOrigin(titleText.getLocalBounds().width / 2, titleText.getLocalBounds().height / 2);
+        titleText.setPosition(window.getSize().x / 2, titleText.getLocalBounds().height / 2);
+
+        sf::Text nextText;
+        nextText.setFont(font);
+        nextText.setString("Click anywhere to continue.");
+        nextText.setCharacterSize(25);
+        nextText.setFillColor(sf::Color::Red);
+        nextText.setOrigin(nextText.getLocalBounds().width / 2, nextText.getLocalBounds().height / 2);
+        nextText.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+
+        auto vis1 = std::make_shared<QuickVisualizer>(&window, &font, true); // right side
+        auto vis2 = std::make_shared<MergeVisualizer>(&window, &font, false); // left side
+
+        bool move_to_results = false;
+        while (window.isOpen() && !move_to_results) {
+            bool vis1Done = vis1->isDone();
+            bool vis2Done = vis2->isDone();
+
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+                if (event.type == sf::Event::MouseButtonPressed && vis1Done && vis2Done)
+                    move_to_results = true;
+            }
+
+            if (!vis1Done)
+                vis1->iterate();
+
+            if (!vis2Done)
+                vis2->iterate();
+
+
+            window.clear(sf::Color::Black);
+
+            window.draw(titleText);
+            vis1->draw();
+            vis2->draw();
+            if (vis1Done && vis2Done)
+                window.draw(nextText);
+
+            window.display();
+        }
+    }
+
+    { // COMPARING WINDOW
+        window.setTitle("Sorting Comparisons");
+
+        sf::Text text1("Original unsorted", font, 30);
+        sf::Text text2("Already sorted", font, 30);
+        sf::Text text3("Reverse sorted", font, 30);
+
+        float windowWidth = window.getSize().x;
+        float windowHeight = window.getSize().y;
+        float yPosition = windowHeight - 100;
+
+        float spacing = windowWidth / 4;
+        text1.setPosition(spacing - text1.getLocalBounds().width / 2 - 75, yPosition);
+        text2.setPosition(2 * spacing - text2.getLocalBounds().width / 2, yPosition);
+        text3.setPosition(3 * spacing - text3.getLocalBounds().width / 2 + 75, yPosition);
+
+        text1.setFillColor(sf::Color::White);
+        text2.setFillColor(sf::Color::White);
+        text3.setFillColor(sf::Color::White);
+
+        sf::Text label1("Merge   vs.   Quick", font, 25);
+        sf::Text label2("Merge   vs.   Quick", font, 25);
+        sf::Text label3("Merge   vs.   Quick", font, 25);
+
+        float labelOffset = 60;
+
+        label1.setPosition(spacing - label1.getLocalBounds().width / 2 - 75, yPosition - labelOffset);
+        label2.setPosition(2 * spacing - label2.getLocalBounds().width / 2, yPosition - labelOffset);
+        label3.setPosition(3 * spacing - label3.getLocalBounds().width / 2 + 75, yPosition - labelOffset);
+
+        label1.setFillColor(sf::Color::White);
+        label2.setFillColor(sf::Color::White);
+        label3.setFillColor(sf::Color::White);
+
+        sf::Text time1("Time", font, 20);
+        sf::Text memory1("Memory", font, 20);
+        sf::Text time2("Time", font, 20);
+        sf::Text memory2("Memory", font, 20);
+        sf::Text time3("Time", font, 20);
+        sf::Text memory3("Memory", font, 20);
+        sf::Text time4("Time", font, 20);
+        sf::Text memory4("Memory", font, 20);
+        sf::Text time5("Time", font, 20);
+        sf::Text memory5("Memory", font, 20);
+        sf::Text time6("Time", font, 20);
+        sf::Text memory6("Memory", font, 20);
+
+        float timeMemoryOffset = 60;
+        float columnOffset = 50;
+
+        time1.setPosition(spacing - label1.getLocalBounds().width / 2 - 75 - columnOffset - 7, yPosition - labelOffset - timeMemoryOffset);
+        memory1.setPosition(spacing - label1.getLocalBounds().width / 2 - 75 - columnOffset + 53, yPosition - labelOffset - timeMemoryOffset);
+        time2.setPosition(spacing - label1.getLocalBounds().width / 2 - 75 - columnOffset + 173, yPosition - labelOffset - timeMemoryOffset);
+        memory2.setPosition(spacing - label1.getLocalBounds().width / 2 - 75 - columnOffset + 233, yPosition - labelOffset - timeMemoryOffset);
+        time3.setPosition(2 * spacing - label1.getLocalBounds().width / 2 - columnOffset - 7, yPosition - labelOffset - timeMemoryOffset);
+        memory3.setPosition(2 * spacing - label1.getLocalBounds().width / 2 - columnOffset + 53, yPosition - labelOffset - timeMemoryOffset);
+        time4.setPosition(2 * spacing - label1.getLocalBounds().width / 2 - columnOffset + 173, yPosition - labelOffset - timeMemoryOffset);
+        memory4.setPosition(2 * spacing - label1.getLocalBounds().width / 2 - columnOffset + 233, yPosition - labelOffset - timeMemoryOffset);
+        time5.setPosition(3 * spacing - label1.getLocalBounds().width / 2 + 75 - columnOffset - 7, yPosition - labelOffset - timeMemoryOffset);
+        memory5.setPosition(3 * spacing - label1.getLocalBounds().width / 2 + 75 - columnOffset + 53, yPosition - labelOffset - timeMemoryOffset);
+        time6.setPosition(3 * spacing - label1.getLocalBounds().width / 2 + 75 - columnOffset + 173, yPosition - labelOffset - timeMemoryOffset);
+        memory6.setPosition(3 * spacing - label1.getLocalBounds().width / 2 + 75 - columnOffset + 233, yPosition - labelOffset - timeMemoryOffset);
+
+        time1.setFillColor(sf::Color::White);
+        memory1.setFillColor(sf::Color::White);
+        time2.setFillColor(sf::Color::White);
+        memory2.setFillColor(sf::Color::White);
+        time3.setFillColor(sf::Color::White);
+        memory3.setFillColor(sf::Color::White);
+        time4.setFillColor(sf::Color::White);
+        memory4.setFillColor(sf::Color::White);
+        time5.setFillColor(sf::Color::White);
+        memory5.setFillColor(sf::Color::White);
+        time6.setFillColor(sf::Color::White);
+        memory6.setFillColor(sf::Color::White);
+
+        while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            window.clear(sf::Color::Black);
+
+            window.draw(text1);
+            window.draw(text2);
+            window.draw(text3);
+            window.draw(label1);
+            window.draw(label2);
+            window.draw(label3);
+            window.draw(time1);
+            window.draw(memory1);
+            window.draw(time2);
+            window.draw(memory2);
+            window.draw(time3);
+            window.draw(memory3);
+            window.draw(time4);
+            window.draw(memory4);
+            window.draw(time5);
+            window.draw(memory5);
+            window.draw(time6);
+            window.draw(memory6);
+            // ADD BAR GRAPHS
+
+            window.display();
         }
 
-        if (!vis1->isDone())
-            vis1->iterate();
-
-        if (!vis2->isDone())
-            vis2->iterate();
-
-        window.clear(sf::Color::Black);
-        vis1->draw();
-        vis2->draw();
-        window.display();
+        return 0;
     }
 
-    /*
-    try {
-        std::vector<DataPoint> data = loadData();
-        testSortPerformance(data);
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
-     */
-
-    // Comparison window
-    sf::RenderWindow resultWindow(sf::VideoMode(1200, 800), "Sorting Comparisons");
-
-    sf::Text text1("Original unsorted", font, 30);
-    sf::Text text2("Already sorted", font, 30);
-    sf::Text text3("Reverse sorted", font, 30);
-
-    float windowWidth = resultWindow.getSize().x;
-    float windowHeight = resultWindow.getSize().y;
-    float yPosition = windowHeight - 100;
-
-    float spacing = windowWidth / 4;
-    text1.setPosition(spacing - text1.getLocalBounds().width / 2 - 75, yPosition);
-    text2.setPosition(2 * spacing - text2.getLocalBounds().width / 2, yPosition);
-    text3.setPosition(3 * spacing - text3.getLocalBounds().width / 2 + 75, yPosition);
-
-    text1.setFillColor(sf::Color::White);
-    text2.setFillColor(sf::Color::White);
-    text3.setFillColor(sf::Color::White);
-
-    sf::Text label1("Merge   vs.   Quick", font, 25);
-    sf::Text label2("Merge   vs.   Quick", font, 25);
-    sf::Text label3("Merge   vs.   Quick", font, 25);
-
-    float labelOffset = 60;
-
-    label1.setPosition(spacing - label1.getLocalBounds().width / 2 - 75, yPosition - labelOffset);
-    label2.setPosition(2 * spacing - label2.getLocalBounds().width / 2, yPosition - labelOffset);
-    label3.setPosition(3 * spacing - label3.getLocalBounds().width / 2 + 75, yPosition - labelOffset);
-
-    label1.setFillColor(sf::Color::White);
-    label2.setFillColor(sf::Color::White);
-    label3.setFillColor(sf::Color::White);
-
-    sf::Text time1("Time", font, 20);
-    sf::Text memory1("Memory", font, 20);
-    sf::Text time2("Time", font, 20);
-    sf::Text memory2("Memory", font, 20);
-    sf::Text time3("Time", font, 20);
-    sf::Text memory3("Memory", font, 20);
-    sf::Text time4("Time", font, 20);
-    sf::Text memory4("Memory", font, 20);
-    sf::Text time5("Time", font, 20);
-    sf::Text memory5("Memory", font, 20);
-    sf::Text time6("Time", font, 20);
-    sf::Text memory6("Memory", font, 20);
-
-    float timeMemoryOffset = 60;
-    float columnOffset = 50;
-
-    time1.setPosition(spacing - label1.getLocalBounds().width / 2 - 75 - columnOffset - 7, yPosition - labelOffset - timeMemoryOffset);
-    memory1.setPosition(spacing - label1.getLocalBounds().width / 2 - 75 - columnOffset + 53, yPosition - labelOffset - timeMemoryOffset);
-    time2.setPosition(spacing - label1.getLocalBounds().width / 2 - 75 - columnOffset + 173, yPosition - labelOffset - timeMemoryOffset);
-    memory2.setPosition(spacing - label1.getLocalBounds().width / 2 - 75 - columnOffset + 233, yPosition - labelOffset - timeMemoryOffset);
-    time3.setPosition(2*spacing - label1.getLocalBounds().width / 2 - columnOffset - 7, yPosition - labelOffset - timeMemoryOffset);
-    memory3.setPosition(2*spacing - label1.getLocalBounds().width / 2 - columnOffset + 53, yPosition - labelOffset - timeMemoryOffset);
-    time4.setPosition(2*spacing - label1.getLocalBounds().width / 2 - columnOffset + 173, yPosition - labelOffset - timeMemoryOffset);
-    memory4.setPosition(2*spacing - label1.getLocalBounds().width / 2 - columnOffset + 233, yPosition - labelOffset - timeMemoryOffset);
-    time5.setPosition(3*spacing - label1.getLocalBounds().width / 2 + 75 - columnOffset - 7, yPosition - labelOffset - timeMemoryOffset);
-    memory5.setPosition(3*spacing - label1.getLocalBounds().width / 2 + 75 - columnOffset + 53, yPosition - labelOffset - timeMemoryOffset);
-    time6.setPosition(3*spacing - label1.getLocalBounds().width / 2 + 75 - columnOffset + 173, yPosition - labelOffset - timeMemoryOffset);
-    memory6.setPosition(3*spacing - label1.getLocalBounds().width / 2 + 75 - columnOffset + 233, yPosition - labelOffset - timeMemoryOffset);
-
-    time1.setFillColor(sf::Color::White);
-    memory1.setFillColor(sf::Color::White);
-    time2.setFillColor(sf::Color::White);
-    memory2.setFillColor(sf::Color::White);
-    time3.setFillColor(sf::Color::White);
-    memory3.setFillColor(sf::Color::White);
-    time4.setFillColor(sf::Color::White);
-    memory4.setFillColor(sf::Color::White);
-    time5.setFillColor(sf::Color::White);
-    memory5.setFillColor(sf::Color::White);
-    time6.setFillColor(sf::Color::White);
-    memory6.setFillColor(sf::Color::White);
-
-    while (resultWindow.isOpen()) {
-        sf::Event event;
-        while (resultWindow.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                resultWindow.close();
-        }
-
-        resultWindow.clear(sf::Color::Black);
-
-        resultWindow.draw(text1);
-        resultWindow.draw(text2);
-        resultWindow.draw(text3);
-        resultWindow.draw(label1);
-        resultWindow.draw(label2);
-        resultWindow.draw(label3);
-        resultWindow.draw(time1);
-        resultWindow.draw(memory1);
-        resultWindow.draw(time2);
-        resultWindow.draw(memory2);
-        resultWindow.draw(time3);
-        resultWindow.draw(memory3);
-        resultWindow.draw(time4);
-        resultWindow.draw(memory4);
-        resultWindow.draw(time5);
-        resultWindow.draw(memory5);
-        resultWindow.draw(time6);
-        resultWindow.draw(memory6);
-        // ADD BAR GRAPHS
-
-        resultWindow.display();
-    }
-
-    return 0;
 }
